9db86183192c71a0fbb83888ff7020212f74a21e
kaibas01 <kaibas01@allegheny.edu>
2024-03-27 10:24:11 -0400

    changes

--- src/code/main.py
+++ src/code/main.py
@@ -1,7 +1,18 @@
 import timeit
+import csv
+
 
 class Experiment:
     def __init__(self) -> None:
         pass
     
-    def 
+    def for_loop(self):
+        file_path = "numbers.txt"
+        with open(file_path, "r", encoding = "utf-8") as fh:
+            reader = csv.reader(fh)
+        return reader
+
+
+if __name__ == "__main__":
+    experiment = Experiment()
+    print(experiment.for_loop())
