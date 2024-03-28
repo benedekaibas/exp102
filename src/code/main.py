5b5e53f34e825ce6d98c2b34443ba7fae0813c6e
kaibas01 <kaibas01@allegheny.edu>
2024-03-28 13:07:33 -0400

    changes int the code

--- src/code/main.py
+++ src/code/main.py
@@ -4,17 +4,21 @@ import timeit
 class Experiment:
     def __init__(self) -> None:
         pass
-        self.list = list 
-    
-    def for_loop(self):
-        list = [1,2,3,4,5]
+        self.list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 
-        for item in list:
-            item = len(list) / sum(list)
-        return item
+    def for_loop(self,):
+        for item in self.list:
+            length_list = len(self.list)
+            item = (length_list - 1) // 2
+        return self.list[item]
 
+    def while_loop(self):
+        while item in self.list:
+            length_list = len(self.list)
+            item = (length_list - 1) // 2
+        return self.list[item]
 
 if __name__ == "__main__":
     experiment = Experiment()
     print(experiment.for_loop())
-    
+    print(experiment.while_loop())
