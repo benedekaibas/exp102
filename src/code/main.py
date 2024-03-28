967c41a23b594c94d6517f668cdf5d691413db95
kaibas01 <kaibas01@allegheny.edu>
2024-03-28 13:08:21 -0400

    docstrings added

--- src/code/main.py
+++ src/code/main.py
@@ -6,13 +6,15 @@ class Experiment:
         pass
         self.list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 
-    def for_loop(self,):
+    def for_loop(self):
+        """Searching with for loop."""
         for item in self.list:
             length_list = len(self.list)
             item = (length_list - 1) // 2
         return self.list[item]
 
     def while_loop(self):
+        """Searching with while loop."""
         while item in self.list:
             length_list = len(self.list)
             item = (length_list - 1) // 2
